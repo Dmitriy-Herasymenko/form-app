9b4886773a9e81b18f06e4828352d294aa15a946
Dmytro Herasymenko <dmytroherasymenko@Dmytros-MacBook-Pro.local>
2023-10-03 08:32:48 +0300

    add useMemo

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,100 +1,109 @@
-import React, {memo} from 'react';
+import React, { memo, useMemo } from 'react';
 import { useForm, Controller, SubmitHandler, Control } from 'react-hook-form';
 import jsonData from '../data.json';
 import './style.css';
 
 type FormValues = {
-  [key: string]: string | number | boolean;
+    [key: string]: string | number | boolean;
 };
 
 type InputFieldProps = {
-  item: typeof jsonData[number];
-  control: Control<FormValues>;
+    item: typeof jsonData[number];
+    control: Control<FormValues>;
 };
 
-const InputField: React.FC<InputFieldProps> = ({ item, control }) => {
-  return (
-    <Controller
-      name={`field${item.label}`}
-      control={control}
-      defaultValue={String(item.default_value)}
-      render={({ field }) => (
-        <>
-          {item.type === 'text' && (
-            <input
-              type="text"
-              {...field}
-              pattern={item.validation || undefined}
-              className="form-input"
-              value={field.value as string}
-            />
-          )}
-          {item.type === 'longtext' && (
-            <textarea
-              {...field}
-              {...(item.validation && { pattern: new RegExp(item.validation) })}
-              className="form-input"
-              value={field.value as string}
-            />
-          )}
-          {item.type === 'dropdown' && (
-            <select
-              {...field}
-              className="form-select"
-              value={String(field.value)} 
-            >
-              {item.options?.map((option, optionIndex) => (
-                <option key={optionIndex} value={String(option)}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          )}
+const InputField: React.FC<InputFieldProps> = memo(({ item, control }) => {
 
-          {item.type === 'number' && (
-            <input
-              type="number"
-              {...field}
-              min={item.min_value || 0}
-              {...(item.max_value && { max: item.max_value })}
-              className="form-input"
-              value={field.value as number}
-            />
-          )}
-        </>
-      )}
-    />
-  );
-};
+    return (
+        <Controller
+            name={`field${item.label}`}
+            control={control}
+            defaultValue={String(item.default_value)}
+            render={({ field }) => (
+                <>
+                    {item.type === 'text' && (
+                        <input
+                            type="text"
+                            {...field}
+                            pattern={item.validation || undefined}
+                            className="form-input"
+                            value={field.value as string}
+                        />
+                    )}
+                    {item.type === 'longtext' && (
+                        <textarea
+                            {...field}
+                            {...(item.validation && { pattern: new RegExp(item.validation) })}
+                            className="form-input"
+                            value={field.value as string}
+                        />
+                    )}
+                    {item.type === 'dropdown' && (
+                        <select
+                            {...field}
+                            className="form-select"
+                            value={String(field.value)}
+                        >
+                            {item.options?.map((option, optionIndex) => (
+                                <option key={optionIndex} value={String(option)}>
+                                    {option}
+                                </option>
+                            ))}
+                        </select>
+                    )}
+
+                    {item.type === 'number' && (
+                        <input
+                            type="number"
+                            {...field}
+                            min={item.min_value || 0}
+                            {...(item.max_value && { max: item.max_value })}
+                            className="form-input"
+                            value={field.value as number}
+                        />
+                    )}
+                </>
+            )}
+        />
+    );
+});
+
+
 
 const Form: React.FC = () => {
-  const { handleSubmit, control } = useForm<FormValues>();
-  const initialValues: FormValues = {};
+    const { handleSubmit, control } = useForm<FormValues>();
+    const initialValues: FormValues = {};
 
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log(data);
-  };
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log(data);
+    };
 
-  jsonData.forEach((item) => {
-    const fieldName = `field${item.label}`;
-    initialValues[fieldName] = item.default_value || '';
-  });
+    jsonData.forEach((item) => {
+        const fieldName = `field${item.label}`;
+        initialValues[fieldName] = item.default_value || '';
+    });
 
-  return (
-    <div className="bg-gray-800 form-container">
-      <form onSubmit={handleSubmit(onSubmit)} className="form">
-        {jsonData.map((item, index) => (
-          <div key={index}>
-            <label className="form-label">{item.label}</label>
-            <InputField item={item} control={control} />
-          </div>
-        ))}
-        <button type="submit" className="form-button">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    const memoizedInputFields = useMemo(
+        () =>
+            jsonData.map((item, index) => (
+                <div key={index}>
+                    <label className="form-label">{item.label}</label>
+                    <InputField item={item} control={control} />
+                </div>
+            )),
+        [jsonData, control]
+    );
+
+    return (
+        <div className="bg-gray-800 form-container">
+            <form onSubmit={handleSubmit(onSubmit)} className="form">
+                {memoizedInputFields}
+                <button type="submit" className="form-button">
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default memo(Form);
